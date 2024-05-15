e507ba5b9fe31ff45e23a60dbcb9e7eca7ddd762
ygorp <ygor-nogueira@hotmail.com>
2024-05-15 15:47:20 -0300

    criando funcionalidade do formulário e mensagem de sucesso ou erro de envio do email

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const btnAbrir = document.getElementById('btn-menu');
 const menu = document.getElementById('menu-mobile');
 const overlay = document.getElementById('overlay-menu');
 
+const form = document.getElementById('contactForm');
+const successMessage = document.getElementById('successMessage');
+const errorMessage = document.getElementById('errorMessage');
+
 btnAbrir.addEventListener('click', () => {
   menu.classList.toggle('menu-mobile--aberto');
 });
@@ -13,4 +17,25 @@ menu.addEventListener('click', () => {
 
 overlay.addEventListener('click', () => {
     menu.classList.toggle('menu-mobile--aberto');
+});
+
+    
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    const formData = new FormData(form);
+    const response = await fetch(form.action, {
+        method: 'POST',
+        body: formData
+    });
+
+    if (response.ok) {
+        successMessage.style.display = 'block';
+        errorMessage.style.display = 'none';
+        form.reset();
+    } else {
+        errorMessage.style.display = 'block';
+        successMessage.style.display = 'none';
+    }
 });
